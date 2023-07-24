8b2e1a1b648ee8f929bf717a40e53907e5d201a1
kay <kayshiro@live.no>
2023-07-24 11:01:26 +0200

    LazyColumn created

--- app/src/main/java/com/kay/movieapp/MainActivity.kt
+++ app/src/main/java/com/kay/movieapp/MainActivity.kt
@@ -1,13 +1,37 @@
 package com.kay.movieapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kay.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,19 +45,93 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+// this function just activate a lambda function
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MyApp(
     content: @Composable () -> Unit
 ) {
     MovieAppTheme {
-        content()
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    backgroundColor = Color.Magenta,
+                    elevation = 5.dp,
+                ) {
+
+                    Text(
+                        text = "Movies"
+                    )
+                }
+            }
+        ) {
+            content()
+        }
+    }
+}
+
+// TODO: Need to learn how to make surface size match parent
+// Need to wrap it around a row, column or box
+// TODO: Need to learn how to center the text in the middle of the screen
+@Composable
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Dragonball z",
+        "Kay the movie",
+        "Lion King",
+        "Titanic",
+        "Life",
+        "Kien",
+        "De la Riva",
+        "Lasso",
+        "Single leg x"
+    )
+) {
+    Column(
+        modifier = Modifier
+            .padding(12.dp)
+    ) {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(movieTitle = it)
+            }
+        }
     }
 }
 
 @Composable
-fun MainContent() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Hello")
+// We want to create a nice card around each items.
+fun MovieRow(movieTitle: String) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = 6.dp
+    ) {
+        // Movie title
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            // ICON
+            Surface(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                elevation = 4.dp
+            ) {
+                Icon(
+                    imageVector = Icons.Default.AccountBox,
+                    contentDescription = "AccountIcon"
+                )
+
+            }
+            Text(text = movieTitle)
+        }
     }
 }
 
