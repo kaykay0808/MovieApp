8dd7b663434da4c05dc05dd6c5de27f38218bbd5
kay <kayshiro@live.no>
2023-09-18 14:15:14 +0200

    Expand card Info

--- app/src/main/java/com/kay/movieapp/ui/widgets/Widgets.kt
+++ app/src/main/java/com/kay/movieapp/ui/widgets/Widgets.kt
@@ -1,26 +1,40 @@
 package com.kay.movieapp.ui.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import coil.transform.CircleCropTransformation
@@ -34,11 +48,13 @@ fun MovieRow(
     movieInfo: MovieData,
     onItemClicked: (String) -> Unit
 ) {
+    var infoExpanded by remember { mutableStateOf(false) }
+
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
+            // .height(130.dp)
             .clickable { onItemClicked(movieInfo.id) },
         shape = RoundedCornerShape(corner = CornerSize(16.dp)),
         elevation = 6.dp
@@ -64,11 +80,6 @@ fun MovieRow(
                         .build(),
                     contentDescription = "MoviePoster"
                 )
-
-                /*Icon(
-                    imageVector = Icons.Default.AccountBox,
-                    contentDescription = "AccountIcon"
-                )*/
             }
             Column(
                 modifier = Modifier.padding(4.dp)
@@ -84,6 +95,45 @@ fun MovieRow(
                 Text(
                     text = "Released: ${movieInfo.year}",
                     style = MaterialTheme.typography.caption
+                )
+                // Hidden information when arrow is clicked
+                AnimatedVisibility(visible = infoExpanded) {
+                    Column {
+                        Text(
+                            buildAnnotatedString {
+                                withStyle(
+                                    style = SpanStyle(
+                                        color = Color.DarkGray,
+                                        fontSize = 13.sp
+                                    )
+                                ) {
+                                    append("Plot: ")
+                                }
+                                withStyle(
+                                    style = SpanStyle(
+                                        color = Color.DarkGray,
+                                        fontSize = 13.sp,
+                                        fontWeight = FontWeight.Light
+                                    )
+                                ) {
+                                    append(movieInfo.plot)
+                                }
+                            }
+                        )
+                    }
+                }
+                // extension arrow?
+                Icon(
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable { infoExpanded = !infoExpanded },
+                    tint = Color.DarkGray,
+                    imageVector = if (infoExpanded) {
+                        Icons.Filled.KeyboardArrowUp
+                    } else {
+                        Icons.Filled.KeyboardArrowDown
+                    },
+                    contentDescription = "Down Arrow"
                 )
             }
         }
